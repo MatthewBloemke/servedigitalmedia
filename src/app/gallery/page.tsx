be4e7daff004919e5a318f9514968b43476414a0
MatthewBloemke <mattbloemke@gmail.com>
2023-05-26 22:34:35 -0500

    formatting changes

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import webPhoto from '../../../public/website1.png';
 import social1 from '../../../public/social1.png';
 import social3 from '../../../public/social3.png';
+import branding1 from '../../../public/branding1.png';
 import branding2 from '../../../public/general2.png';
 import branding3 from '../../../public/general3.png';
 import branding4 from '../../../public/general4.png';
@@ -14,8 +15,10 @@ import Link from 'next/link';
 const Gallery = () => {
   return (
     <div className="flex flex-col">
-      <div id="videoProjects" className="my-12">
-        <h1 className="ml-[5%]">Video Projects</h1>
+      <div id="videoProjects" className=" flex flex-col mt-3 items-center">
+        <div className="flex w-full">
+          <h1 className="font-semibold text-2xl ml-[5%]">Video Projects</h1>
+        </div>
         <div className="flex flex-col md:flex-row items-center justify-evenly flex-wrap">
           <div className="relative mt-5">
             <div className="absolute bg-gradient-to-b from-[#ff8c52] w-[70px] h-[50px] top-[-5px] left-[-5px] z-[-1] rounded-sm" />
@@ -51,7 +54,7 @@ const Gallery = () => {
             ></iframe>
           </div>
         </div>
-        <div className="border-2 border-white w-[80%] mx-auto mt-5 rounded-md relative bg-black/40">
+        <div className="border-2 border-white w-[90%] mx-1 mt-5 rounded-md relative bg-black/40">
           <p className="text-6xl absolute top-[-15px] left-[-2px]">&quot;</p>
           <p className="text-6xl absolute bottom-[-41px] right-[-1px]">
             &quot;
@@ -63,13 +66,17 @@ const Gallery = () => {
               him again.
             </p>
           </div>
-          <div className="flex w-[80%] justify-end">
+          <div className="flex w-[90%] justify-end">
             <h4 className="md:text-2xl">-Dan</h4>
           </div>
         </div>
       </div>
-      <div id="webProjects" className="my-12">
-        <h1 className="ml-[5%]">Web Design Projects</h1>
+      <div id="webProjects" className="flex flex-col my-12 items-center">
+        <div className="flex w-full">
+          <h1 className="ml-[5%] text-2xl font-semibold">
+            Web Design Projects
+          </h1>
+        </div>
         <div className="flex flex-col md:flex-row justify-center items-center">
           <div className="relative mt-5 w-[80%] md:w-[40%]">
             <Link href="https://matthewbloemke.com" target="_blank">
@@ -90,13 +97,13 @@ const Gallery = () => {
           </p>
           <div className="flex justify-center items-center w-full text-center">
             <p className="md:text-lg m-2">
-              Here is a nice long example about why Samuel is the greatest to
-              work with. He is very professional and punctual, and is great to
-              work with.
+              Matt did an awesome job creating our inventory program. It has
+              worked flawlessly and is a huge time saver. We would definitely
+              recommend him for any software design applications.
             </p>
           </div>
           <div className="flex w-[80%] justify-end">
-            <h4 className="md:text-2xl">-Person</h4>
+            <h4 className="md:text-2xl">-Dale</h4>
           </div>
         </div>
       </div>
@@ -152,11 +159,11 @@ const Gallery = () => {
         <h1 className="ml-[5%]">General Branding Projects</h1>
         <div className="flex flex-col">
           <div className="flex flex-col md:flex-row justify-evenly items-center">
-            <div className="relative mt-5 w-[80%] md:w-[30%] bg-white flex justify-center align-center rounded-sm">
+            <div className="relative mt-5 w-[80%] md:w-[30%] bg-black flex justify-center align-center rounded-sm">
               <div className="absolute bg-gradient-to-b from-[#ff8c52] w-[70px] h-[50px] top-[-5px] left-[-5px] z-[-1] rounded-sm" />
               <div className="absolute bg-gradient-to-t from-[#ff8c52] w-[70px] h-[50px] bottom-[-5px] right-[-5px] z-[-1] rounded-sm" />
               <Image
-                src={placeHolder}
+                src={branding1}
                 className="max-h-[300px] w-auto m-auto rounded-sm"
                 alt="general branding photo"
               />
